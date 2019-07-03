3553ebb213c449da7c7236efaa2a25197dc63ede
Quinton Smith <smith.quinton42@gmail.com>
2019-07-03 12:20:41 -0400

    NaN does not show up anymore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,47 +49,54 @@ $("#gem-total").text(goalTotal);
 var yourTotal = 0;
 
 var garnetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var addGarnetNumbers = garnetNumbers[Math.floor(Math.random() * 12) + 1];
+var addGarnetNumbers = garnetNumbers[Math.floor(Math.random() * 12)];
 
 $("#Garnet").on("click", function(){
         // var random = Math.floor(Math.random() * 12) + 1;
         //  garnetNumbers;
         console.log(garnetNumbers);
-        // alert(yourTotal += addGarnetNumbers);
+        console.log(yourTotal += addGarnetNumbers);
         console.log(yourTotal);
         $("#your-total").text(yourTotal);
     });
 
 var stevenNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var addStevenNumbers = stevenNumbers[Math.floor(Math.random() * 12) + 1];
+var addStevenNumbers = stevenNumbers[Math.floor(Math.random() * 12)];
 
 $("#Steven").on("click", function() {
     console.log(stevenNumbers);
-    // alert(yourTotal += addStevenNumbers);
+    console.log(yourTotal += addStevenNumbers);
     console.log(yourTotal);
     $("#your-total").text(yourTotal);
 });
 
 var amethystNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var addAmethystNumbers = amethystNumbers[Math.floor(Math.random() * 12) + 1];
+var addAmethystNumbers = amethystNumbers[Math.floor(Math.random() * 12)];
 
 $("#Amethyst").on("click", function() {
     console.log(amethystNumbers);
-    // alert(yourTotal += addAmethystNumbers);
+    console.log(yourTotal += addAmethystNumbers);
     console.log(yourTotal);
     $("#your-total").text(yourTotal);
 });
 
 var pearlNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var addPearlNumbers = pearlNumbers[Math.floor(Math.random() * 12) + 1];
+var addPearlNumbers = pearlNumbers[Math.floor(Math.random() * 12)];
 
 $("#Pearl").on("click", function() {
     console.log(pearlNumbers);
-    // alert(yourTotal += addPearlNumbers);
+    console.log(yourTotal += addPearlNumbers);
     console.log(yourTotal);
     $("#your-total").text(yourTotal);
 });
 
+if (yourTotal === goalTotal) {
+    alert("You win!");
+  }
+
+  else if (yourTotal > goalTotal) {
+    alert("You lose!!");
+  };
 // // THIRD TIME'S THE CHARM
 // var goalTotal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 // $("#gem-total").text(goalTotal);
